03103674e437b289961ea291009c083bb084d4af
marcelobacher <63582097+marcelobacher@users.noreply.github.com>
2021-02-24 14:58:25 +0200

    Add files via upload
    Bugfixing distance computation

--- code/coreset_selection.m
+++ code/coreset_selection.m
@@ -34,4 +34,4 @@ function [psi,d] = l_calc_psi(C, X)
 psi = sum(d);
 
 function [L, d] = l_calc_dist(C, X)
-[d,L] = max(abs(bsxfun(@minus, 2.0*real(X*C'), dot(C, C, 2).')), [], 2);
+[d,L] = max(bsxfun(@minus, 2.0*real(X*C'), dot(C, C, 2).'), [], 2);
